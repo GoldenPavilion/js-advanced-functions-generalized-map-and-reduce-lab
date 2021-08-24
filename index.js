ed07cd2e2970abc57dee55a32a0685c930649da7
GoldenPavilion <rcorystine@gmail.com>
2021-08-23 21:38:33 -0400

    updated map and reduce functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions here
+function map(sourceArray, func){
+    let array = []
+        for (let i = 0; i < sourceArray.length; i++){
+            let element = sourceArray[i]
+            array.push(func(element))
+        }
+    return array
+}
+
+function reduce (sourceArray, func, startingValue) {
+    let tempValue;
+
+    if(startingValue) {
+        tempValue = startingValue;
+        for (let i = 0; i < sourceArray.length; i++) {
+            tempValue = func(sourceArray[i], tempValue);
+        }
+    }
+
+    else {
+        tempValue = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++) {
+            tempValue = func(sourceArray[i], tempValue);
+        }
+    }
+    return tempValue;
+}
